2bd7186bb030f70f16beb64cb69f59aa9701486a
ghilger16 <ghilger16@yahoo.com>
2021-01-03 16:18:50 -0700

    added calculate functions

--- script.js
+++ script.js
@@ -3,59 +3,28 @@
 let calculator = document.querySelector(".calculator");
 let calculatorDisplay = document.getElementById("display");
 let previousKey = "";
-// document
-//   .getElementById("zero")
-//   .addEventListener("click", () => displayNumber(0));
+let firstValue = "";
+let operator = "";
+let secondValue = "";
 
-// document
-//   .getElementById("one")
-//   .addEventListener("click", () => displayNumber(1));
-
-// document
-//   .getElementById("two")
-//   .addEventListener("click", () => displayNumber(2));
-
-// document
-//   .getElementById("three")
-//   .addEventListener("click", () => displayNumber(3));
-
-// document
-//   .getElementById("four")
-//   .addEventListener("click", () => displayNumber(4));
-
-// document
-//   .getElementById("five")
-//   .addEventListener("click", () => displayNumber(5));
-
-// document
-//   .getElementById("six")
-//   .addEventListener("click", () => displayNumber(6));
-
-// document
-//   .getElementById("seven")
-//   .addEventListener("click", () => displayNumber(7));
-
-// document
-//   .getElementById("eight")
-//   .addEventListener("click", () => displayNumber(8));
-
-// document
-//   .getElementById("nine")
-//   .addEventListener("click", () => displayNumber(9));
 document.querySelectorAll(".calculator").forEach((item) => {
   item.addEventListener("click", (e) => {
     if (e.target.className === "operator") {
       previousKey = "operator";
+    } else if (e.target.className === "decimal") {
+      previousKey = "decimal";
+    } else if (e.target.className === "calculate") {
+      previousKey = "calculate";
     } else {
       previousKey = "digit";
     }
+    console.log(previousKey);
   });
 });
 
 document.querySelectorAll(".digits").forEach((item) => {
   item.addEventListener("click", (e) => {
-    const key = e.target;
-    const keyContent = key.textContent;
+    const keyContent = e.target.textContent;
     if (calculatorDisplay.innerText === "0" || previousKey === "operator") {
       calculatorDisplay.innerText = keyContent;
     } else {
@@ -66,6 +35,40 @@ document.querySelectorAll(".digits").forEach((item) => {
   });
 });
 
-document.querySelectorAll(".operator").forEach((item) => {
-  item.addEventListener("click", (e) => {});
+document.querySelector(".decimal").addEventListener("click", () => {
+  if (!calculatorDisplay.innerText.includes(".")) {
+    calculatorDisplay.innerText = calculatorDisplay.innerText + ".";
+  } else if (previousKey === "operator" || previousKey === "calculate") {
+    calculatorDisplay.innerText = "0";
+  }
 });
+
+document.querySelectorAll(".operator").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    firstValue = calculatorDisplay.textContent;
+    operator = e.target.id;
+    console.log(firstValue);
+    console.log(operator);
+  });
+});
+
+document.querySelector(".calculate").addEventListener("click", () => {
+  if (firstValue) {
+    secondValue = calculatorDisplay.textContent;
+  }
+  calculatorDisplay.textContent = calculate(firstValue, operator, secondValue);
+});
+
+let calculate = function (firstValue, operator, secondValue) {
+  let result = "";
+  if (operator === "plus") {
+    result = parseFloat(firstValue) + parseFloat(secondValue);
+  } else if (operator === "minus") {
+    result = parseFloat(firstValue) - parseFloat(secondValue);
+  } else if (operator === "multiply") {
+    result = parseFloat(firstValue) * parseFloat(secondValue);
+  } else if (operator === "divide") {
+    result = parseFloat(firstValue) / parseFloat(secondValue);
+  }
+  return result;
+};
